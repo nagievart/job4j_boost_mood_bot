4dadc5552a4c47f715554eccbfa8ab1a452f0c33
Артур Нагиев <nagiev-art@mail.ru>
2026-03-10 17:58:15 +0300

    -

--- src/main/java/ru/job4j/bmb/services/MoodService.java
+++ src/main/java/ru/job4j/bmb/services/MoodService.java
@@ -1,4 +1,19 @@
 package ru.job4j.bmb.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MoodService {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed now.");
+    }
 }
--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -1,5 +1,7 @@
 package ru.job4j.bmb.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
@@ -13,5 +15,15 @@ public class TelegramBotService {
 
     public void receive(Content content) {
         handler.receive(content);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed now.");
     }
 }
